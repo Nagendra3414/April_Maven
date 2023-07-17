dc01cbca9ef0828ed183ea4683110c30c128ac4f
Nagendra3414 <gadi.nagendra12@gmail.com>
2023-07-17 08:23:52 +0530

    Gowthami created new class in Branch1

---
+++ src/test/java/branch1/BranchClass1.java
@@ -0,0 +1,22 @@
+package branch1;
+
+public class BranchClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Branch 1");
+
+		System.out.println("Branch 1");
+
+		System.out.println("Branch 1");
+
+		System.out.println("Branch 1");
+		System.out.println("Branch 1");
+		System.out.println("Branch 1");
+
+		System.out.println("Branch 1");
+		System.out.println("Branch 1");
+	}
+
+}
